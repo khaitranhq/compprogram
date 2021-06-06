4f2a67e2375927e6a9bd1a88109d7cd33fa4d5c8
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-06-06 16:31:11 +0700

    change commits

--- Codeforces/CFR510/D/data.cpp
+++ Codeforces/CFR510/D/data.cpp
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-PROBLEM_NAME="CFR510/D"
-
-if [ -d "$PROBLEM_NAME" ]; then
-  echo "Direction existed"
-  exit 1
-fi
-
-mkdir -p $PROBLEM_NAME
-
-cd $PROBLEM_NAME
-cat > data.cpp <<- "EOF"
 #include <bits/stdc++.h>
 
 #define debug(x) cout << #x << " = " << x << endl;
@@ -44,6 +31,37 @@ using namespace std;
 typedef int64_t ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
+
+struct Query {
+  ll index, k;
+
+  Query() {}
+  Query(ll index, ll k) : index(index), k(k) {}
+
+  bool operator<(const Query &other) { return this->k > other.k; }
+};
+
+const int MAX = 2e5 + 5;
+int n;
+ll t;
+ll a[MAX], prefixSum[MAX];
+
+struct FenwickTree {
+  ll Tree[MAX];
+
+  void update(int u, int x) {
+    for (; u <= n; u += u & -u)
+      Tree[u] += x;
+  }
+
+  ll get(int u) {
+    ll ans = 0;
+    for (; u; u -= u & -u)
+      ans += Tree[u];
+    return ans;
+  }
+} BIT;
 
 int main() {
 #ifdef LOCAL
@@ -51,11 +69,34 @@ int main() {
   freopen("data.out", "w", stdout);
 #endif
 
+  cin >> n >> t;
+
+  ll res = 0;
+  vector<pll> indexValue;
+  vector<Query> queries;
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+
+    prefixSum[i] = prefixSum[i - 1] + a[i];
+    if (prefixSum[i] - t < 0)
+      ++res;
+
+    indexValue.push_back({prefixSum[i], i});
+    queries.push_back(Query(i, prefixSum[i] - t));
+  }
+
+  sort(indexValue.begin(), indexValue.end());
+  sort(queries.begin(), queries.end());
+
+  for (auto query : queries) {
+    while (indexValue.size() && indexValue.back().first > query.k) {
+      BIT.update(indexValue.back().second, 1);
+      indexValue.pop_back();
+    }
+
+    res += BIT.get(query.index - 1);
+  }
+  cout << res;
+
   return 0;
 }
-EOF
-
-touch data.inp
-touch data.out
-
-echo "Create problem successfully"
