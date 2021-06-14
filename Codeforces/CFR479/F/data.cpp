1abade30df77a65598c6c592864b6f2ece8cd0cb
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-06-14 23:43:41 +0700

    change commits

--- Codeforces/CFR479/F/data.cpp
+++ Codeforces/CFR479/F/data.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <vector>
 
 #define debug(x) cout << #x << " = " << x << endl;
 #define debugarr2d(x, n, m)                                                    \
@@ -35,6 +39,34 @@ typedef pair<int, int> pii;
 const int MAX = 2e5 + 5;
 int n;
 
+struct FenwickTree {
+  int Tree[MAX];
+  int n;
+
+  void update(int u, int x) {
+    for (; u <= n; u += u & -u)
+      Tree[u] += x;
+  }
+
+  int get(int u) {
+    int ans = 0;
+    for (; u; u -= u & -u)
+      ans += Tree[u];
+    return ans;
+  }
+
+  FenwickTree(int n) { this->n = n; }
+};
+
+vector<int> findLongestIncreasingSubsequence(vector<int> arr) {
+  int largest_element = *max_element(arr.begin(), arr.end());
+
+  FenwickTree BIT(largest_element);
+
+  vector<int> ans;
+  return ans;
+}
+
 int main() {
 #ifdef LOCAL
   freopen("data.inp", "r", stdin);
@@ -42,7 +74,25 @@ int main() {
 #endif
 
   cin >> n;
-  for (int i = 0; i < n; ++i) {
+  vector<pii> a;
+  for (int i = 1; i <= n; ++i) {
+    int x;
+    cin >> x;
+    a.push_back({x, i});
   }
+
+  sort(a.begin(), a.end());
+
+  vector<int> index_of_increase_values;
+  index_of_increase_values.push_back(a[0].second);
+  for (int i = 1; i < a.size(); ++i) {
+    if (abs(a[i].first - a[i - 1].first) > 1) {
+      findLongestIncreasingSubsequence(index_of_increase_values);
+      index_of_increase_values.clear();
+    }
+
+    index_of_increase_values.push_back(a[i].second);
+  }
+  findLongestIncreasingSubsequence(index_of_increase_values);
   return 0;
 }
