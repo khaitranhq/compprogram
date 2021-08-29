d00ae6b6b3bb89d4a3217111e2fad5d2c2e13b00
lioaslan <leoalan5577@gmail.com>
2021-08-29 16:55:40 +0700

    Update script

--- Codeforces/initCompetitive.sh
+++ Codeforces/initCompetitive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTEST_NAME="CFR/CFR739"
+CONTEST_NAME="CFR/CFR740"
 PROBLEMS=(A B C D E F G)
 
 if [ -d "$CONTEST_NAME" ]; then
@@ -11,9 +11,10 @@ fi
 mkdir $CONTEST_NAME
 
 cd $CONTEST_NAME
-for problemName in "${PROBLEMS[@]}" do 
-  mkdir $problemName 
-  cd $problemName 
+for problemName in "${PROBLEMS[@]}" 
+do 
+  mkdir $problemName
+  cd $problemName
 
   wget https://raw.githubusercontent.com/lioaslan/Competitive-Training/master/sample.cpp
   mv sample.cpp data.cpp
