0ebb8267a7a108c08cc754c6e894e60a1572bb64
Khai Tran <leoalan5577@gmail.com>
2024-03-24 23:01:53 +0700

    feat: Add largestPalindromic method and test cases

---
+++ Leetcode/largest-palindromic-number/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string largestPalindromic(string num) {
+        
+    }
+};
+
+int main() {
+	Solution solution;
+
+
+}
