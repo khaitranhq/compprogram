2d7f309f8e63dbff691e2747b9d0171d37015901
Khai Tran <leoalan5577@gmail.com>
2024-03-26 16:32:58 +0700

    test: Update test input and output file paths

--- Leetcode/largest-palindromic-number/main.cpp
+++ Leetcode/largest-palindromic-number/main.cpp
@@ -1,15 +1,43 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    string largestPalindromic(string num) {
-        
+  string largestPalindromic(string num) {
+    vector<int> digitCount(10, 0);
+    for (int i = 0; i < num.size(); ++i)
+      digitCount[num[i] - '0']++;
+
+    int middleDigit = -1;
+    string halfResult = "";
+    for (int i = 9; i >= 0; --i) {
+      if (i != 0 || (i == 0 & halfResult.size() != 0))
+        halfResult.append(digitCount[i] / 2, i + '0');
+      if (( digitCount[i] % 2 || (halfResult.size() == 0 && i == 0) ) && middleDigit == -1)
+        middleDigit = i;
     }
+
+    string result = halfResult;
+    if (middleDigit != -1)
+      result.append(to_string(middleDigit));
+    reverse(halfResult.begin(), halfResult.end());
+    result.append(halfResult);
+
+    return result;
+  }
 };
 
 int main() {
-	Solution solution;
+  string inputNum;
+  cin >> inputNum;
 
+  Solution solution;
 
+  string result = solution.largestPalindromic(inputNum);
+  cout << result;
+
+  return 0;
 }
