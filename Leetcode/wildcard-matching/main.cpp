9c7caa30bb1a94b912087754b03edbacecaeda9f
Khai Tran <khaitran.lewis@gmail.com>
2024-10-13 14:05:49 +0700

    ♻️ (wildcard-matching): refactor isMatch function to improve readability and efficiency by using recursion and memoization ✅ (wildcard-matching): add test cases to validate the refactored isMatch function

--- Leetcode/wildcard-matching/main.cpp
+++ Leetcode/wildcard-matching/main.cpp
@@ -1,47 +1,105 @@
+#include <cstring>
+#include <iostream>
+#include <map>
+
+#define debug(x) cout << #x << "=" << x << endl
+using namespace std;
+
 class Solution {
 public:
   bool isMatch(string s, string p) {
-      // Anchor case
-    if (mp.count({s, p}))
-      return mp[{s, p}];
-
-    if (p == "*")
-      return true;
-
-    if (s.size() == 0 && p.size() == 0)
-      return true;
-
-    if (s.size() == 1 && p.size() == 1) {
-      mp[{s, p}] = ((s[0] == p[0]) || (p[0] == '?'));
-      return mp[{s, p}];
-    }
-
-    // Recursive case
-    if (p[0] == '?') {
-      mp[{s, p}] = s.size() > 0 && isMatch(s.substr(1), p.substr(1));
-      return mp[{s, p}];
-    }
-
-    if (p[0] == '*') {
-      bool ans = isMatch(s, p.substr(1));
-      for (int i = 0; i < s.size(); ++i)
-        ans |= isMatch(s.substr(i + 1), p.substr(1));
-      mp[{s, p}] = ans;
-      return ans;
-    }
-
-    bool ans = (s[0] == p[0]);
-    if (!ans) {
-      mp[{s, p}] = false;
-      return false;
-    }
-
-    ans &= isMatch(s.substr(1), p.substr(1));
-    mp[{s, p}] = ans;
-    return ans;
-
+    str = s;
+    pattern = p;
+    return isMatchRecursive(0, 0);
   }
 
 private:
-  map<pair<string, string>, bool> mp;
+  const static int MAX = 2005;
+  string str, pattern;
+  map<pair<int, int>, int> f;
+
+  bool isMatchRecursive(int startSIndex, int startPIndex) {
+    debug(startSIndex);
+    debug(startPIndex);
+    // Anchor case
+    if (pattern == "*")
+      return true;
+
+    if (str.size() == startSIndex && pattern.size() == startPIndex)
+      return true;
+
+    if (f.count({startSIndex, startPIndex}))
+      return f[{startSIndex, startPIndex}];
+
+    if (str.size() - 1 == startSIndex && pattern.size() - 1 == startPIndex) {
+      f[{startSIndex, startPIndex}] =
+          ((str[startPIndex] == pattern[startPIndex]) ||
+           (pattern[startPIndex] == '?'));
+      return f[{startSIndex, startPIndex}];
+    }
+
+    // Recursive case
+    if (pattern[0] == '?') {
+      f[{startSIndex, startPIndex}] =
+          startSIndex < str.size() &&
+          isMatchRecursive(startSIndex + 1, startPIndex + 1);
+      return f[{startSIndex, startPIndex}];
+    }
+
+    if (pattern[0] == '*') {
+      bool ans = isMatchRecursive(startSIndex, startPIndex + 1);
+      for (int i = 0; i < str.size() && !ans; ++i)
+        ans |= isMatchRecursive(startSIndex + i + 1, startPIndex + 1);
+      f[{startSIndex, startPIndex}] = ans;
+      return ans;
+    }
+
+    bool ans = (str[0] == pattern[0]);
+    if (!ans) {
+      f[{startSIndex, startPIndex}] = false;
+      return false;
+    }
+
+    ans &= isMatchRecursive(startSIndex + 1, startPIndex + 1);
+    f[{startSIndex, startPIndex}] = ans;
+    return ans;
+  }
 };
+
+void expect(string test, string s, string p, bool expected) {
+  Solution sol;
+  bool result = sol.isMatch(s, p);
+  if (result == expected) {
+    cout << "\033[1m"
+         << "\033[32m"
+         << "PASS: " << test << "\033[0m"
+         << "\033[0m" << endl;
+  } else {
+    cout << "\033[1m"
+         << "\033[31m"
+         << "FAIL: " << test << "\033[0m"
+         << "\033[0m" << endl;
+    cout << "-----------------------------" << endl;
+    cout << "result: " << result << endl;
+    cout << "-----------------------------" << endl;
+    cout << "expected: " << expected << endl;
+    cout << "-----------------------------" << endl;
+    exit(1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  string s, p;
+
+  // s = "baabbabababaabbabababbaabbbbaaabaaabbbbaaaaaabbbbaaabaaabbbbbabaabbbbbbb"
+  //     "babbbabbabbbbabbbbabbbbbbabababbaaaabbbbaabaaababbbabaaaabaabbbabbaabbab"
+  //     "bbbabaababbbbbbbabbaaaabaaabbaaabaaaaababbbaaaabbbbbabbabb";
+  // p = "ba*ba*bb*a********abaa*bb**abb**b***ab**b*b*babb***a*bb*aaabb*****b*"
+  //     "aabb**aa**b*a***b*bb*b*bb*a*bbbbb**";
+  // expect("Test 1", s, p, true);
+
+  s = "aa";
+  p = "a";
+  expect("Test 2", s, p, false);
+  return 0;
+}
