5a9c614874021b17ea9e45242982460c85df2354
Khai Tran <khaitran.lewis@gmail.com>
2025-08-10 09:32:12 +0700

    feat(DIGOKEYS): implement BFS algorithm to find the path between boxes in the DIGOKEYS problem on SPOJ

---
+++ SPOJ/DIGOKEYS/main.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> bfs(int startBoxIndex, int targetBoxIndex,
+                vector<vector<int>> &openableBoxes) {
+  queue<int> q;
+  vector<int> beforeBox(openableBoxes.size(), -1);
+  q.push(startBoxIndex);
+  beforeBox[startBoxIndex] = startBoxIndex;
+
+  while (!q.empty()) {
+    int currentBox = q.front();
+    q.pop();
+
+    if (currentBox == targetBoxIndex) {
+      // Reconstruct path
+      vector<int> path;
+      int current = targetBoxIndex;
+      while (current != startBoxIndex) {
+        path.push_back(current);
+        current = beforeBox[current];
+      }
+      path.push_back(startBoxIndex);
+      reverse(path.begin(), path.end());
+      return path;
+    }
+
+    for (int box : openableBoxes[currentBox]) {
+      if (beforeBox[box] == -1) {
+        beforeBox[box] = currentBox;
+        q.push(box);
+      }
+    }
+  }
+
+  // No path found
+  return vector<int>();
+}
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+
+    // Initialize openableBoxes with n+1 elements for 1-based indexing
+    vector<vector<int>> openableBoxes(n + 1);
+
+    // Read keys for boxes 1 to n-1 (box n doesn't have a key since it's the
+    // target)
+    for (int i = 1; i < n; ++i) {
+      int m;
+      cin >> m;
+      for (int j = 0; j < m; ++j) {
+        int box;
+        cin >> box;
+        openableBoxes[i].push_back(box);
+      }
+    }
+
+    // Find path from box 1 to box n
+    vector<int> pathToBox = bfs(1, n, openableBoxes);
+    if (pathToBox.empty()) {
+      cout << "-1" << endl;
+    } else {
+      cout << pathToBox.size() - 1 << endl; // Number of steps (edges)
+      for (int i = 0; i < pathToBox.size() - 1; ++i) {
+        cout << pathToBox[i] << " ";
+      }
+      cout << endl;
+    }
+  }
+  return 0;
+}
